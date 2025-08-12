adc1977a629d4426790ff3abbe0d71fab4567086
Nikita Doroshenko <nikitokd05@gmail.com>
2025-08-12 15:11:38 +0300

    fix solution

---
+++ src/components/Clock/Clock.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+type Props = {
+  clockName: string;
+};
+
+type State = {
+  today: Date;
+};
+
+export class Clock extends React.Component<Props, State> {
+  state: Readonly<State> = {
+    today: new Date(),
+  };
+
+  messageId = 0;
+
+  componentDidMount(): void {
+    this.messageId = window.setInterval(() => {
+      const now = new Date();
+
+      // eslint-disable-next-line no-console
+      console.log(now);
+      this.setState({ today: now });
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Readonly<Props>): void {
+    if (this.props.clockName !== prevProps.clockName) {
+      // eslint-disable-next-line no-console
+      console.warn(
+        `Renamed from ${prevProps.clockName} to ${this.props.clockName}`,
+      );
+    }
+  }
+
+  componentWillUnmount(): void {
+    window.clearInterval(this.messageId);
+  }
+
+  render() {
+    const { today } = this.state;
+    const { clockName } = this.props;
+
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">{clockName}</strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {today.toUTCString().slice(-12, -4)}
+        </span>
+      </div>
+    );
+  }
+}
